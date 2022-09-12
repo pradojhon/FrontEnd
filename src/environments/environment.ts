9e3e4515d81a1db19a5a9c2fadc002e6baf631ee
pradojhon <113234308+pradojhon@users.noreply.github.com>
2022-09-12 10:54:36 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'http://roljhonprado-001-site1.htempurl.com',
+    issuer: 'http://roljhonprado-001-site1.atempurl.com',
     redirectUri: baseUrl,
     clientId: 'BookStore_App',
     responseType: 'code',
@@ -19,7 +19,7 @@ export const environment = {
   },
   apis: {
     default: {
-      url: 'http://roljhonprado-001-site1.htempurl.com',
+      url: 'http://roljhonprado-001-site1.atempurl.com,
       rootNamespace: 'Acme.BookStore',
     },
   },
